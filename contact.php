23591280ea7753fef4c7a85cd8b11b93f4da7b31
mbaveo <39050834+mbaveo@users.noreply.github.com>
2018-09-10 15:03:28 +0530

    cssphphtml

--- contact.php
+++ contact.php
@@ -1,21 +1,24 @@
-<?php
 
-if (isset($_POST['submit']) ) {
-	# code..
-	$name = $_POST['name'];
-$email = $_POST['email'];
-$msg = $_POST['mssg'];
+<?php 
 
-$mailTo = "maithri.my301@gmail.com";
-$headers = "From: " .$email;
-$txt = "You have recieved an email from " .$name.".\n\n".$mssg;
+$mailto = "maithri.my301@gmail.com";
+$page = "index.html";
 
-mail($mailTo, $txt, $headers);
-header("Location: index.php");
+$name = $_POST["name"];
+$email = $_POST["email"];
+$msg = $_POST["mssg"];
 
-}
+$name = htmlentities($name,ENT_QUOTES, "UTF-8");
+$email = htmlentities($email,ENT_QUOTES, "UTF-8");
+$msg = htmlentities($msg,ENT_QUOTES, "UTF-8");
 
+$name = str_replace("\r\n",'"', $name);
+$email = str_replace("\r\n",'"', $email);
+$msg = str_replace("\r\n",'\t', $msg);
+$msg = str_replace("\r",'\t', $msg);
+$msg = str_replace("\n",'\t', $msg);
 
-
+echo "Thank You! $name We will get back to you soon!"
 
 ?>
+
